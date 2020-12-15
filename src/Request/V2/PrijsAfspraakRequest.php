228ec522a6c26c32e1b4ab97264797373415b94d
Jochem Lettink <jochem@doitonlinemedia.nl>
2020-12-15 12:38:03 +0100

    fix for aantal that was being converted to binary numbers

--- src/Request/V2/PrijsAfspraakRequest.php
+++ src/Request/V2/PrijsAfspraakRequest.php
@@ -20,7 +20,7 @@ final class PrijsAfspraakRequest extends BaseRequest
     public static function getByArticleAndCustomer(Artikel $artikel, Relatie $relatie, $aantal = 1)
     {
         return new Request('GET',
-            sprintf('prijsafspraken/explicit-parameters?artikelPublicIdentifier=%s&aantal=%b&relatiePublicIdentifier=%s',
+            sprintf('prijsafspraken/explicit-parameters?artikelPublicIdentifier=%s&aantal=%s&relatiePublicIdentifier=%s',
                 $artikel->getId()->toString(), $aantal, $relatie->getId()->toString()));
     }
 
